15bf5eb3ce83e3b81d1fcd074be77b84c5faa036
Atendra <aduve@akamai.com>
2023-02-27 17:32:26 +0530

    --continue impl

--- run_aggregations.py
+++ run_aggregations.py
@@ -24,7 +24,7 @@ import logging
 import time
 import json
 import os
-import azure.cosmos.cosmos_client as cosmos_client
+#import azure.cosmos.cosmos_client as cosmos_client
 
 from aggregation_modules.aggregator import Aggregator
 
